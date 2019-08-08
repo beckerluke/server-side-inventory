88c071d1b8ea21df6b15782410978d76a4feb70e
Luke Becker <beckerluke1@gmail.com>
2019-08-08 16:21:12 -0500

    Completed post request and appended listOfInventory to the DOM. Reorganized functions in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,49 @@
 $(document).ready(onReady);
 
 function onReady() {
+    $('#js-btn-add-inventory').on('submit', submitInventory);
+    
+    getInventory();
+}
+
+function submitInventory() {
+    event.preventDefault(); 
+
+    const formArray = $(this).serializeArray(); 
+    const dataForServer = {
+        name: $('#js-form-foodName').val(),
+        description: $('#js-form-description').val(),
+    }; 
 
     $.ajax({
-        type: 'GET',
-        url: '/inventory'
-        }).then(function(response) {
-        console.log(response);
+        type: 'POST',
+        url: '/inventory',
+        data: dataForServer,
+    }).then(function(response) {
+        getInventory();
     });
+}
+
+function getInventory() { 
+    console.log('in getInventory');
+    
+    $.ajax({
+        type: 'GET',
+        url: '/inventory',
+        }).then(function(response) {
+        render(response);
+    }); 
+}
+
+function render(response){
+    const listOfInventory = response;
+    $('.js-container').empty();
+    for (let food of listOfInventory) {
+        // console.log(food.name);
+        $('.js-container').append(`
+         <div>
+             <p>${food.name} - ${food.description}</p>
+         </div>
+        `)
+    }
 }
--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const app = express();
 const PORT = 5000; 
 
 const inventoryList = require('./modules/inventory'); 
+const bodyParser = require('body-parser');
 
 app.use(express.static('server/public')); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log('Server is On!');
@@ -14,4 +17,8 @@ app.get('/inventory', (req, res) => {
     res.send(inventoryList);
 });
 
-
+app.post('/inventory', (req, res) => {
+    const newInventory = req.body;
+    inventoryList.push(newInventory);
+    res.send('Received newInventory')
+});
